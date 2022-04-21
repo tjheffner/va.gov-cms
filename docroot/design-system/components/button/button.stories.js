7c80ca079d352791f865a3d0d08a170be6123807
Tanner Heffner <tanner.heffner@agile6.com>
2022-04-21 12:18:51 -0700

    VACMS-8682 import claro styles to sb for parity. add buttons

--- docroot/design-system/components/button/button.stories.js
+++ docroot/design-system/components/button/button.stories.js
@@ -2,7 +2,11 @@ import button from './sb-buttons.twig';
 import './button.scss';
 import './index.js';
 
+<<<<<<< HEAD
 export default { title: 'Atoms/Buttons', component: button };
+=======
+export default { title: 'Atoms/Button', component: button };
+>>>>>>> 67ebf0e9 (VACMS-8682 import claro styles to sb for parity. add buttons)
 
 export const Buttons = {
   args: {},
